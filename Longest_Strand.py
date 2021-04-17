ccc7ab981efae23c3c08e5951a223bc44951798e
syk535 <48624546+syk535@users.noreply.github.com>
2021-04-17 18:29:02 -0400

    Add files via upload

--- Longest_Strand.py
+++ Longest_Strand.py
@@ -52,24 +52,23 @@ def largest_save(sample_1, sample_2):
     
 # Read files in a way which can be extended to a large number of files with the name structure    
 sample=list()
-for i in range(1,11):
+total=10
+for i in range(1,total+1):
     name="sample."+str(i)
     with open(name,'rb') as file:
         sample_sub=file.read()
     sample.append(sample_sub)
 
-max_all=np.zeros((10,10))
+max_all=np.zeros((total,total))
 place_all=list()
 
 # Find the longest strand between each pair of files
 # Because the longest strand of bytes that exists in at least two files must be one of the longest strands between all pairs of files
-for num_1  in range(9):
-    for num_2 in range(num_1+1,10):
-        print(num_1+1,num_2+1)
+for num_1  in range(total-1):
+    for num_2 in range(num_1+1,total):
         max_i, place_i=largest_save(sample[num_1], sample[num_2])
         max_all[num_1,num_2]=max_i
         place_all.append(place_i)
-        print(max_i)
 
 # Analyze the longest strands between all pairs of files
 max_len=max_all.max()
@@ -77,12 +76,12 @@ pairs=np.where(max_all==max_len)
 num_pairs=len(pairs[0])
 num_unique_largest_strand=1
 files_selected=[pairs[0][0]+1,pairs[1][0]+1]
-place_selected=int(files_selected[0]*19/2-files_selected[0]**2/2+files_selected[1]-11)
 output=open('Output.txt','w')
 
 # To discuss how many pairs to achieve the largest length and whether these longest strands are the same.
 # If we have multiple distinct longest strands, it will show the corresponding files and offets, respectively.
 if num_pairs==1 and len(place_all[place_selected])==1:
+    place_selected=int(files_selected[0]*(2*total-1)/2-files_selected[0]**2/2+files_selected[1]-11)
     offset_1=[place_all[place_selected][0][0]-max_len+2,place_all[place_selected][0][0]+1]
     offset_2=[place_all[place_selected][0][1]-max_len+2,place_all[place_selected][0][1]+1]
     output.write('We have '+str(num_unique_largest_strand)+' unique largest strand.'+'\n')
@@ -113,7 +112,7 @@ else:
             strings.append(string_new)
     for k in range(1,num_pairs):
         files_selected=[pairs[0][k]+1,pairs[1][k]+1]
-        place_selected=int(files_selected[0]*19/2-files_selected[0]**2/2+files_selected[1]-11)
+        place_selected=int(files_selected[0]*(2*total-1)/2-files_selected[0]**2/2+files_selected[1]-11)
         g=len(place_all[place_selected])
         for i in range(g):
             offset1_new=['Sample.'+str(pairs[0][k])+1,place_all[place_selected][i][0]-max_len+2,place_all[place_selected][i][0]+1]
